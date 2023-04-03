520a6cedca9dc0a023b9392122e207c3af79237d
Lucy Zhai <zz3635@nyu.edu>
2023-04-03 12:29:16 -0400

    Player name change and code change

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -21,7 +21,7 @@ public class PlayerScript : MonoBehaviour
     {
 
         Debug.Log(playerName);
-        playerName = "Link";
+        playerName = "Yellow";
         playerLevel = 0;
     }
 
@@ -33,9 +33,9 @@ public class PlayerScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Enemy")
+        if(collision.name == "Enemy")
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
             //add code to restart game later
             //reset exp gain?
         }
