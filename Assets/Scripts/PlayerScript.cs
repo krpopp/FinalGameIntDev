819a130eb5cb5e2d95589de9c6e64d2e86e79408
Lucy Zhai <zz3635@nyu.edu>
2023-04-03 12:17:01 -0400

    Script and sprite change for the player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -19,6 +19,8 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        Debug.Log(playerName);
         playerName = "Link";
         playerLevel = 0;
     }
